fc47a7c9a74a3ac025deab3561b29bbadc78e6e3
ranjan1996kumar@gmail.com <ranjan1996kumar@gmail.com>
2021-08-18 20:07:01 +0530

    Q6-Compute Quotient and Remainder

---
+++ QuotientRemainder.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class QuotientRemainder {
+    public static void main(String[] args) {
+
+        // Creating int variable for no 1
+        int no1 = 200;
+        // Creating int variable for no 2
+        int no2 = 40;
+
+        // Calculating the quotient
+        int quotient = no1 / no2;
+        // Calculating the remainder
+        int remainder = no1 % no2;
+
+        System.out.println("Quotient value: "+quotient);
+        System.out.println("Remainder value: "+remainder);
+    }
+}
